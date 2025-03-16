9631364b114333b5e95f12c196ec442f60556f25
Shreesha1564 <147400183+Shreesha1564@users.noreply.github.com>
2025-03-17 00:32:39 +0530

    changes done for deployment

--- backend/index.js
+++ backend/index.js
@@ -7,13 +7,14 @@ import userRoute from "./routes/user.route.js"
 import companyRoute from "./routes/company.route.js"
 import jobRoute from "./routes/job.route.js"
 import applicationRoute from "./routes/application.route.js"
-
+import path from "path";
 dotenv.config({});
 
 
 const app = express();
 
 // -- draft --
+const __dirname = path.resolve();
 app.get("/home" , (req , res) =>{
     return res.status(200).json({
         message: "I am coming from backend",
@@ -41,7 +42,10 @@ app.use("/api/v1/company",companyRoute);
 app.use("/api/v1/job",jobRoute);
 app.use("/api/v1/application",applicationRoute);
 
-
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (_, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+});
 // examples of how api will be made
 // "http://localhost:8000/api/v1/user/register"
 // "http://localhost:8000/api/v1/user/login"
